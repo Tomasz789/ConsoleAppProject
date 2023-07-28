8873d13a73f011c0e0dd6390dcaab52d92fc618e
Tomasz789 <lachowiec_95@wp.pl>
2023-07-28 12:22:02 +0200

    Uploaded tests

---
+++ TextConsoleApp/TextApp.Tests/TextLibTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextApp.Tests
+{
+    [TestFixture]
+    public class TextLibTests
+    {
+        [Test]
+        public void Assertion()
+        {
+            Assert.Pass();
+        }
+    }
+}
