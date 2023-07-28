ee699851c155920b82141de257e4f2603c086d64
Tomasz789 <lachowiec_95@wp.pl>
2023-07-28 11:18:49 +0200

    Uploaded text library

---
+++ TextConsoleApp/ConsoleApp.TextLib/TextClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.TextLib
+{
+    public static class TextClass
+    {
+        public static int CountCharacters(string text)
+        {
+            return 0;
+        }
+    }
+}
